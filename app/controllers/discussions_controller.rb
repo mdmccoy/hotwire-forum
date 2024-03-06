38cfe6a6440570458a7c2ed86b363ec4ecf060ba
Matt McCoy <mdmccoy@gmail.com>
2024-03-06 16:31:22 -0500

    Add discussions form

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -8,4 +8,24 @@ class DiscussionsController < ApplicationController
   def new
     @discussion = Discussion.new
   end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+    @discussion.user = current_user
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to discussions_path, notice: 'Discussion was successfully created.' }
+      else
+        flash[:alert] = 'Discussion could not be created.'
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :pinned, :closed)
+  end
 end
