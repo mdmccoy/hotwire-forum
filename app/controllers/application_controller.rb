50a07d1878c49de32ed41017eba822e06718a121
Matt McCoy <mdmccoy@gmail.com>
2024-03-06 15:49:52 -0500

    Add Current, rubocop.yml, rails-rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_current_user, if: :user_signed_in?
+
+  private
+
+  def set_current_user
+    Current.user = current_user
+  end
 
   protected
 
