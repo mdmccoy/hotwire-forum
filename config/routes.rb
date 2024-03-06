bcadeadd9998723b45c3b8fbd79472aa8cb68afc
Matt McCoy <mdmccoy@gmail.com>
2024-03-06 16:14:30 -0500

    Discussion index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :discussions, only: [:index, :new]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: 'main#index'
