cc7ceabd7f47a9eaea397487c58fa50ff26ab509
Matt McCoy <mdmccoy@gmail.com>
2025-01-15 16:25:11 -0500

    Realtime editing of posts

--- app/controllers/discussions/posts_controller.rb
+++ app/controllers/discussions/posts_controller.rb
@@ -4,19 +4,39 @@ module Discussions
   class PostsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_discussion
+    before_action :set_post, only: %i[show edit update]
+
+    def show; end
+
+    def edit; end
 
     def create
       @post = @discussion.posts.new(post_params)
 
       respond_to do |format|
         if @post.save
-          format.html { redirect_to @discussion }
+          format.html { redirect_to @discussion, notice: 'Post Created!' }
         else
           format.turbo_stream do
             # This response can also be defined in <controller method name>.turbo_stream.erb
             # render turbo_stream: turbo_stream.replace(@post, partial: 'discussions/posts/form', locals: { post: @post })
           end
           format.html { render :new, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def update
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to @post.discussion, notice: 'Post Updated!' }
+        else
+          # @post.broadcast_replace(partial: 'discussions/posts/post', locals: { post: @post })
+          # format.turbo_stream do
+          #   # This response can also be defined in <controller method name>.turbo_stream.erb
+          #   # render turbo_stream: turbo_stream.replace(@post, partial: 'discussions/posts/form', locals: { post: @post })
+          # end
+          format.html { render :edit, status: :unprocessable_entity }
         end
       end
     end
@@ -31,6 +51,10 @@ module Discussions
 
     def set_discussion
       @discussion = Discussion.find(params[:discussion_id])
+    end
+
+    def set_post
+      @post = @discussion.posts.find(params[:id])
     end
 
     def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :discussions do
-    resources :posts, only: %i[create destroy], module: :discussions
+    resources :posts, only: %i[create show edit update destroy], module: :discussions
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
