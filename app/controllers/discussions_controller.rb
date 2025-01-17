7d8a3925ed98faabc704b82c9ce1a0e9a42a4570
Matt McCoy <mdmccoy@gmail.com>
2025-01-17 18:32:33 -0500

    Realtime changing of discussion categories

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -40,6 +40,20 @@ class DiscussionsController < ApplicationController
     respond_to do |format|
       if @discussion.update(discussion_params)
         @discussion.broadcast_replace(partial: 'discussions/header', discussion: @discussion)
+
+        if @discussion.saved_change_to_category_id?
+          old_category_id, new_category_id = @discussion.saved_change_to_category_id
+
+          old_category = Category.find(old_category_id)
+          new_category = Category.find(new_category_id)
+
+          @discussion.broadcast_remove_to(old_category)
+          @discussion.broadcast_prepend_to(new_category)
+
+          old_category.reload.broadcast_replace_to('categories')
+          new_category.reload.broadcast_replace_to('categories')
+        end
+
         format.html { redirect_to @discussion, notice: 'Discussion was successfully updated.' }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Discussion < ApplicationRecord
-  include TurboStreamConcern
-
   belongs_to :user, default: -> { Current.user }
   belongs_to :category, counter_cache: true, touch: true
   has_many :posts, dependent: :destroy
@@ -17,6 +15,12 @@ class Discussion < ApplicationRecord
   delegate :name, prefix: :category, to: :category, allow_nil: true
 
   broadcasts_to :category # , inserts_by: :prepend # this is the default behavior
+
+  after_create_commit { broadcast_prepend_to('discussions') }
+  after_update_commit do
+    broadcast_replace_to('discussions')
+  end
+  after_destroy_commit { broadcast_remove_to('discussions') }
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...20]}".parameterize
