100c6ccbb1509a1652179a5ee9c7b06c6abf6535
Matt McCoy <mdmccoy@gmail.com>
2025-01-19 16:52:31 -0500

    Refactor DiscussionsController#update

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -19,10 +19,42 @@ class Discussion < ApplicationRecord
   after_create_commit { broadcast_prepend_to('discussions') }
   after_update_commit do
     broadcast_replace_to('discussions')
+    replace_headers
+    move_categories if saved_change_to_category_id?
+    update_new_post_form if saved_change_to_closed?
   end
   after_destroy_commit { broadcast_remove_to('discussions') }
 
   def to_param
     "#{id}-#{name.downcase.to_s[0...20]}".parameterize
+  end
+
+  private
+
+  def replace_headers
+    broadcast_replace(partial: 'discussions/header', discussion: self)
+  end
+
+  def move_categories
+    old_category_id, new_category_id = saved_change_to_category_id
+
+    old_category = Category.find(old_category_id)
+    new_category = Category.find(new_category_id)
+
+    broadcast_remove_to(old_category)
+    broadcast_prepend_to(new_category)
+
+    old_category.reload.broadcast_replace_to('categories')
+    new_category.reload.broadcast_replace_to('categories')
+  end
+
+  def update_new_post_form
+    broadcast_action_to(
+      self,
+      action: :replace,
+      target: 'new_post_form',
+      partial: 'discussions/posts/form',
+      locals: { post: posts.new }
+    )
   end
 end
